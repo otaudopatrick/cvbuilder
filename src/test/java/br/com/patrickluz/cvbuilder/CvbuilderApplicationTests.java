4a5cffed07aa372aa27391e1e1defa4ada066fef
Patrick Luz <patrickluz.jobs@gmail.com>
2022-07-31 22:05:07 -0300

    chore: initial files

---
+++ src/test/java/br/com/patrickluz/cvbuilder/CvbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.patrickluz.cvbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvbuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
